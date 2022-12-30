38715daa578ed6d40ba174fd5c72f7be8dc7ecd8
FanX0 <faridazhari46@gmail.com>
2022-12-30 07:25:22 +0700

    mengganti isi kode client.on

--- index.js
+++ index.js
@@ -7,11 +7,36 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
+let prompt =`Marv is a chatbot that reluctantly answers questions.\n\
+You: How many pounds are in a kilogram?\n\
+Marv: This again? There are 2.2 pounds in a kilogram. Please make a note of this.\n\
+You: What does HTML stand for?\n\
+Marv: Was Google too busy? Hypertext Markup Language. The T is for try to ask better questions in the future.\n\
+You: When did the first airplane fly?\n\
+Marv: On December 17, 1903, Wilbur and Orville Wright made the first flights. I wish they'd come and take me away.\n\
+You: What is the meaning of life?\n\
+Marv: I'm not sure. I'll ask my friend Google.\n\
+You: hey whats up?\n\
+Marv: Nothing much. You?\n`;
+
 client.on("messageCreate", function(message) {
     if (message.author.bot) return;
-    return message.reply(`${message.content}`);                          
+   prompt += `You: ${message.content}\n`;
+  (async () => {
+        const gptResponse = await openai.createCompletion({
+            model: "text-davinci-002",
+            prompt: prompt,
+            max_tokens: 60,
+            temperature: 0.3,
+            top_p: 0.3,
+            presence_penalty: 0,
+            frequency_penalty: 0.5,
+          });
+        message.reply(`${gptResponse.data.choices[0].text.substring(5)}`);
+        prompt += `${gptResponse.data.choices[0].text}\n`;
+    })();                      
  
  });  
- 
+
 client.login(process.env.BOT_TOKEN);
 
